a32b6a14387eb6a85c16fc2a2b20e7c65ab348b3
michael370662 <michael370662@users.noreply.github.com>
2020-11-18 15:07:05 -0500

    Update minabssum.cpp

--- minabssum.cpp
+++ minabssum.cpp
@@ -7,6 +7,31 @@
  #include <iterator>
 // you can write to stdout for debugging purposes, e.g.
 // cout << "this is a debug message" << endl;
+// This solution cannot handle 1,2,2,3. So back to the drawing board
+// Following comment is the only solution thus far using brute force travesing to every node. 
+// Solution is from YotKay from stackoverflow
+// So far even a unordered set to delete pair of number will not work to improve runtime because 1,2,2,3 case exists
+// (2,2) cannot be deleted as (1+3) - (2+2) is a valid answer. 
+//#include <vector>
+//using namespace std;
+
+//int sum(vector<int>& A, size_t i, int s)
+//{
+//   if (i == A.size())     
+//      return s;
+
+//   int tmpl = s + A[i];
+//   int tmpr = s - A[i];
+//   return min (abs(sum(A, i+1, tmpl)), abs(sum(A, i+1, tmpr)));
+//}
+
+//int solution(vector<int> &A) {
+//   return sum(A, 0, 0);   
+//}
+
+
+
+
 int minimum(std::vector<int> &A, int& i)
 {
     if(i < A.size()-1){
